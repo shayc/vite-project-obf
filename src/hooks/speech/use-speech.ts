1716036fbf9126f3ca1bf69e9cac383963cdbb57
Shay Cojocaru <shayc@outlook.com>
2024-04-21 01:42:47 +0300

    .

--- src/hooks/speech/use-speech.ts
+++ src/hooks/speech/use-speech.ts
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 export const useSpeech = () => {
+  const [volume, setVolume] = useState(1);
+  const [rate, setRate] = useState(1);
+  const [pitch, setPitch] = useState(1);
+
   const speak = (text: string) => {
     const utterance = new SpeechSynthesisUtterance(text);
+
+    utterance.volume = volume;
+    utterance.rate = rate;
+    utterance.pitch = pitch;
 
     speechSynthesis.speak(utterance);
   };
 
-  return { speak };
+  return { speak, setVolume, setRate, setPitch, volume, rate, pitch };
 };
