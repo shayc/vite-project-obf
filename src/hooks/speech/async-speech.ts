e619d9635fbb3080308b37f9e498b0834226e474
Shay Cojocaru <shayc@outlook.com>
2024-04-23 18:47:55 +0300

    .

--- src/hooks/speech/async-speech.ts
+++ src/hooks/speech/async-speech.ts
@@ -1,14 +1,11 @@
-// Volume ranges from 0 to 1, default is 1
 export const MIN_VOLUME = 0;
 export const MAX_VOLUME = 1;
 export const DEFAULT_VOLUME = 1;
 
-// Rate ranges from 0 to 2, default is 1
 export const MIN_RATE = 0;
 export const MAX_RATE = 2;
 export const DEFAULT_RATE = 1;
 
-// Pitch ranges from 0 to 2, default is 1
 export const MIN_PITCH = 0;
 export const MAX_PITCH = 2;
 export const DEFAULT_PITCH = 1;
