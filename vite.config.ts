3687d9f8d8615bb6b33c7e608d5873bd2e8283a8
Shay Cojocaru <shayc@outlook.com>
2024-05-16 01:38:58 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [react()],
   test: {
     globals: true,
