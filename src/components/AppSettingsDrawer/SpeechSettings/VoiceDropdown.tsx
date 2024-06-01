5ae477ac8af1b9c49da4132ad5141c6db651289d
Shay Cojocaru <shayc@outlook.com>
2024-06-01 15:05:22 +0300

    .

--- src/components/AppSettingsDrawer/SpeechSettings/VoiceDropdown.tsx
+++ src/components/AppSettingsDrawer/SpeechSettings/VoiceDropdown.tsx
@@ -3,6 +3,8 @@ import {
   Label,
   Option,
   OptionGroup,
+  OptionOnSelectData,
+  SelectionEvents,
   makeStyles,
   tokens,
   useId,
@@ -23,14 +25,27 @@ export function VoiceDropdown() {
   const { groupedVoices, setSelectedVoiceURI } = useSpeech();
   const voiceId = useId();
 
+  function handleVoiceSelect(
+    _ev: SelectionEvents,
+    data: OptionOnSelectData,
+  ): void {
+    setSelectedVoiceURI(data.optionValue ?? "");
+  }
+
   return (
     <div className={classes.root}>
       <Label htmlFor={voiceId}>Voice</Label>
-      <Dropdown id={voiceId} placeholder="Select a voice">
+      <Dropdown
+        id={voiceId}
+        placeholder="Select a voice"
+        onOptionSelect={handleVoiceSelect}
+      >
         {Object.entries(groupedVoices).map(([lang, voices]) => (
           <OptionGroup key={lang} label={lang}>
-            {voices?.map((voice) => (
-              <Option key={voice.name}>{voice.name}</Option>
+            {voices.map(({ name, voiceURI }) => (
+              <Option key={voiceURI} value={voiceURI}>
+                {`${name}`}
+              </Option>
             ))}
           </OptionGroup>
         ))}
