5a18dbcb15f4c507925cad27b1ad480103ddb924
Shay Cojocaru <shayc@outlook.com>
2024-06-12 20:17:38 +0300

    .

--- src/components/Board/BoardButton/BoardButton.tsx
+++ src/components/Board/BoardButton/BoardButton.tsx
@@ -22,14 +22,8 @@ const useStyles = makeStyles({
       cursor: "default",
     },
 
-    ":not(:disabled):active::after": {
-      content: "' '",
-      position: "absolute",
-      top: "0",
-      left: "0",
-      right: "0",
-      bottom: "0",
-      backgroundColor: "rgba(0, 0, 0, 0.3)",
+    ":active": {
+      filter: "brightness(0.8)",
     },
   },
 });
