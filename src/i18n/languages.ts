a55559772270260557a587a178db2547c9793d3f
Shay Cojocaru <shayc@outlook.com>
2024-06-24 00:06:13 +0300

    .

---
+++ src/i18n/languages.ts
@@ -0,0 +1,298 @@
+export const languages = [
+  {
+    lang: "en",
+    name: "English",
+  },
+  {
+    lang: "it",
+    name: "italiano",
+  },
+  {
+    lang: "sv",
+    name: "svenska",
+  },
+  {
+    lang: "fr",
+    name: "français",
+  },
+  {
+    lang: "ms",
+    name: "Melayu",
+  },
+  {
+    lang: "de",
+    name: "Deutsch",
+  },
+  {
+    lang: "he",
+    name: "עברית",
+  },
+  {
+    lang: "id",
+    name: "Indonesia",
+  },
+  {
+    lang: "bg",
+    name: "български",
+  },
+  {
+    lang: "es",
+    name: "español",
+  },
+  {
+    lang: "fi",
+    name: "suomi",
+  },
+  {
+    lang: "pt",
+    name: "português",
+  },
+  {
+    lang: "nl",
+    name: "Nederlands",
+  },
+  {
+    lang: "ja",
+    name: "日本語",
+  },
+  {
+    lang: "ro",
+    name: "română",
+  },
+  {
+    lang: "th",
+    name: "ไทย",
+  },
+  {
+    lang: "hr",
+    name: "hrvatski",
+  },
+  {
+    lang: "sk",
+    name: "slovenčina",
+  },
+  {
+    lang: "hi",
+    name: "हिन्दी",
+  },
+  {
+    lang: "uk",
+    name: "українська",
+  },
+  {
+    lang: "zh",
+    name: "中文",
+  },
+  {
+    lang: "vi",
+    name: "Tiếng Việt",
+  },
+  {
+    lang: "ar",
+    name: "العربية",
+  },
+  {
+    lang: "el",
+    name: "Ελληνικά",
+  },
+  {
+    lang: "ru",
+    name: "русский",
+  },
+  {
+    lang: "ca",
+    name: "català",
+  },
+  {
+    lang: "nb",
+    name: "norsk bokmål",
+  },
+  {
+    lang: "da",
+    name: "dansk",
+  },
+  {
+    lang: "hu",
+    name: "magyar",
+  },
+  {
+    lang: "tr",
+    name: "Türkçe",
+  },
+  {
+    lang: "ko",
+    name: "한국어",
+  },
+  {
+    lang: "pl",
+    name: "polski",
+  },
+  {
+    lang: "cs",
+    name: "čeština",
+  },
+  {
+    lang: "af",
+    name: "Afrikaans",
+  },
+  {
+    lang: "sq",
+    name: "shqip",
+  },
+  {
+    lang: "am",
+    name: "አማርኛ",
+  },
+  {
+    lang: "az",
+    name: "azərbaycan",
+  },
+  {
+    lang: "bn",
+    name: "বাংলা",
+  },
+  {
+    lang: "bs",
+    name: "bosanski",
+  },
+  {
+    lang: "my",
+    name: "Burmese",
+  },
+  {
+    lang: "et",
+    name: "eesti",
+  },
+  {
+    lang: "fil",
+    name: "Filipino",
+  },
+  {
+    lang: "gl",
+    name: "Galician",
+  },
+  {
+    lang: "ka",
+    name: "Georgian",
+  },
+  {
+    lang: "gu",
+    name: "ગુજરાતી",
+  },
+  {
+    lang: "is",
+    name: "Icelandic",
+  },
+  {
+    lang: "ga",
+    name: "Irish",
+  },
+  {
+    lang: "jv",
+    name: "Javanese",
+  },
+  {
+    lang: "kn",
+    name: "ಕನ್ನಡ",
+  },
+  {
+    lang: "kk",
+    name: "Kazakh",
+  },
+  {
+    lang: "km",
+    name: "Khmer",
+  },
+  {
+    lang: "lo",
+    name: "Lao",
+  },
+  {
+    lang: "lv",
+    name: "latviešu",
+  },
+  {
+    lang: "lt",
+    name: "lietuvių",
+  },
+  {
+    lang: "mk",
+    name: "Macedonian",
+  },
+  {
+    lang: "ml",
+    name: "മലയാളം",
+  },
+  {
+    lang: "mt",
+    name: "Maltese",
+  },
+  {
+    lang: "mr",
+    name: "मराठी",
+  },
+  {
+    lang: "mn",
+    name: "Mongolian",
+  },
+  {
+    lang: "ne",
+    name: "नेपाली",
+  },
+  {
+    lang: "ps",
+    name: "Pashto",
+  },
+  {
+    lang: "fa",
+    name: "فارسی",
+  },
+  {
+    lang: "sr",
+    name: "српски",
+  },
+  {
+    lang: "si",
+    name: "සිංහල",
+  },
+  {
+    lang: "sl",
+    name: "slovenščina",
+  },
+  {
+    lang: "so",
+    name: "Somali",
+  },
+  {
+    lang: "su",
+    name: "Sundanese",
+  },
+  {
+    lang: "sw",
+    name: "Kiswahili",
+  },
+  {
+    lang: "ta",
+    name: "தமிழ்",
+  },
+  {
+    lang: "te",
+    name: "తెలుగు",
+  },
+  {
+    lang: "ur",
+    name: "اردو",
+  },
+  {
+    lang: "uz",
+    name: "o‘zbek",
+  },
+  {
+    lang: "cy",
+    name: "Welsh",
+  },
+  {
+    lang: "zu",
+    name: "isiZulu",
+  },
+];
